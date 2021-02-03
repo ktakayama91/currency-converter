ca05822b17047ffd6f7eb45052514fa3d4d51cf4
Kei Takayama <ktakayama.91@gmail.com>
2021-02-03 18:46:24 -0500

    Add UT for Currency Use Case class

--- src/test/java/com/bcp/challenge/usecase/business/CurrencyUseCaseImplTest.java
+++ src/test/java/com/bcp/challenge/usecase/business/CurrencyUseCaseImplTest.java
@@ -2,14 +2,20 @@ package com.bcp.challenge.usecase.business;
 
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import java.util.List;
 
 import com.bcp.challenge.domain.CurrencyModel;
+import com.bcp.challenge.domain.ExchangeModel;
+import com.bcp.challenge.usecase.business.response.CurrencyConverterResponse;
 import com.bcp.challenge.usecase.business.response.CurrencyResponse;
 import com.bcp.challenge.usecase.port.output.CurrencyRepositoryPort;
+import com.bcp.challenge.usecase.port.output.ExchangeRepositoryPort;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import io.reactivex.observers.TestObserver;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,6 +36,9 @@ class CurrencyUseCaseImplTest {
 
     @Mock
     private CurrencyRepositoryPort currencyRepositoryPort;
+
+    @Mock
+    private ExchangeRepositoryPort exchangeRepositoryPort;
 
     @Test
     public void findAllSuccessTest() {
@@ -57,6 +66,67 @@ class CurrencyUseCaseImplTest {
         testObserver.assertValueCount(2);
     }
 
+    @Test
+    public void findAllEmptyTest() {
+        List<CurrencyModel> currencyModelList = Arrays.asList();
+        Mockito.when(currencyRepositoryPort.findAll()).thenReturn(currencyModelList);
+        Observable<CurrencyResponse> observable = currencyUseCase.findAll();
 
+        TestObserver<CurrencyResponse> testObserver = new TestObserver<>();
+        observable.subscribe(testObserver);
+
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        testObserver.assertValueCount(0);
+    }
+
+    @Test
+    public void convertSuccessTest() {
+        String currencyFrom = "USD";
+        String currencyTo = "PEN";
+
+        CurrencyModel currencyModelPEN = CurrencyModel
+                .builder()
+                .id(1L)
+                .name("PEN")
+                .build();
+        CurrencyModel currencyModelUSD = CurrencyModel
+                .builder()
+                .id(2L)
+                .name("USD")
+                .build();
+
+        Mockito.when(currencyRepositoryPort.findCurrencyByName(currencyFrom)).thenReturn(currencyModelUSD);
+        Mockito.when(currencyRepositoryPort.findCurrencyByName(currencyTo)).thenReturn(currencyModelPEN);
+
+        ExchangeModel exchangeModel = ExchangeModel
+                .builder()
+                .from(currencyModelUSD)
+                .to(currencyModelPEN)
+                .rate(3.64)
+                .build();
+
+        Mockito.when(exchangeRepositoryPort.findExchangeRateByCurrencyName(currencyModelUSD,
+                currencyModelPEN)).thenReturn(exchangeModel);
+
+        TestObserver<CurrencyConverterResponse> observer = new TestObserver();
+        Single<CurrencyConverterResponse> single = currencyUseCase.convert(100.00, currencyFrom, currencyTo);
+
+        single.subscribe(observer);
+        observer.assertValueCount(1);
+
+        CurrencyConverterResponse currencyConverterResponse = observer.values().get(0);
+        assertEquals(currencyConverterResponse.getExchangeAmount(),364.00);
+    }
+
+    @Test
+    public void convertTest_whenIllegalArgumentException() {
+        String currencyFrom = "USD";
+        String currencyTo = "USD";
+
+        assertThrows(IllegalArgumentException.class,
+                () -> currencyUseCase.convert(100.00, currencyFrom, currencyTo));
+
+    }
 
 }
